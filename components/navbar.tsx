8d6cb5883ad42686c080303611d840eca53aa9cf
BuildTools <84160600+leibovitzeitan@users.noreply.github.com>
2021-06-06 03:49:39 +0300

    Skidadle skidudle your logo is a noodle.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,17 @@
+import Link from "next/link";
 import React, { Component } from "react";
-import Logo from "./Logo"
+import Logo from "./Logo";
 
 export default class Navbar extends Component {
   render() {
-    const routes = [] 
+    const routes = [];
     return (
-      <div className='p-4'>
-        <Logo wsize='w-24' hsize='h-24'></Logo>
+      <div className="p-4">
+        <Link href='/'>
+          <a>
+            <Logo wsize="w-24" hsize="h-24"></Logo>
+          </a>
+        </Link>
       </div>
     );
   }
